ac0179688159d1732ef4290b8240bc80ea469388
Nahuel Quintana <47321353+Nahuellauquintana1@users.noreply.github.com>
2019-02-22 12:30:22 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -4,7 +4,12 @@ function Mostrar()
 acumulador es la suma de 2 variables*/
 //while(contador >5)
 //while(!(contador == 5))
+/* For se utiliza cuando sabemos de antemano la cantidad de veces que se va a repetir el codigo for(var x= 0; x >5; x++) ESTRUCTURA 
+Do while se utiliza cuando queremos que el while se execute almenos una vez y while para cuando no sabemos la cantidad de veces que se tiene que ejecutar el while*/
+
 var contador=0;
+
+
 var acumulador=0;
     	
 		while(contador > 5){
